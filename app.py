ffb5b86347d015af6416c8a4297bad3c93dbfd59
22041174-vic <22041174@itdurango.edu.mx>
2026-08-30 19:10:53 -0600

    Modify Flask app to use environment variables for port
    Updated the app to run on a specified port and host.

--- app.py
+++ app.py
@@ -66,4 +66,5 @@ def listar_alumnos():
 crear_base_datos()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
